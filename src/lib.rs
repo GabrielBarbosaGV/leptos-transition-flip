7214d885fd53f056865160fb3176a8399337c72a
Gabriel Barbosa <gabrielbgasparv@gmail.com>
2023-08-26 22:38:42 +0100

    Adding Mapping trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::{collections::HashMap, hash::Hash};
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+use proptest::proptest;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// Used to map arbitrary keys to values.
+///
+/// The intent of this trait is to be able to map identifiers to each eventual NodeRef,
+/// as, at first, they must be accessed to obtain their positions, and these positions
+/// have to be updated later. To be able to know which NodeRef must be updated with
+/// a given new style, lookup is done through this mapping.
+///
+/// `mapping.get(t)` is used to retrieve an item from the mapping by the key "t".
+///
+/// `mapping.insert(t, u)` is used to associate a key "t" with a value "u".
+///
+/// `mapping.keys()` returns all keys. This is necessary to make sure every NodeRef
+/// gets its style set.
+pub trait Mapping<T, U> {
+    fn get(&self, t: &T) -> Option<&U>;
+
+    fn insert(&mut self, t: T, u: U);
+
+    fn keys(&self) -> Vec<T>;
+}
+
+impl<T, U> Mapping<T, U> for HashMap<T, U> where T: Hash + Eq + Clone + ToOwned<Owned = T> {
+    fn get(&self, t: &T) -> Option<&U> {
+        self.get(t)
     }
+
+    fn insert(&mut self, t: T, u: U) {
+        self.insert(t, u);
+    }
+
+    fn keys(&self) -> Vec<T> {
+        self
+            .keys()
+            .into_iter()
+            .map(|k| k.to_owned())
+            .collect()
+    }
+}
+
+
+
+#[cfg(test)]
+proptest! {
+
 }
