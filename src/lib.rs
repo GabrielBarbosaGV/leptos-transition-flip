e0abbc433bbccf5e5f7b08b12eec70e2cc3ed2b8
Gabriel Barbosa <gabrielbgasparv@gmail.com>
2023-09-17 15:55:39 +0100

    Initial tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,126 +1,312 @@
-use leptos::{html::ElementDescriptor, *};
-use std::{collections::HashMap, fmt::Display, hash::Hash};
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+    hash::Hash,
+};
 
-enum Instruction<'a, T> {
-    ComputePosition(&'a T),
-    DiffPositions(&'a T, &'a T),
-}
-
-impl<T> Display for Instruction<'_, T>
-where
-    T: Display,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::ComputePosition(t) => write!(f, "Instruction::ComputePosition({})", t),
-            Self::DiffPositions(old, new) => write!(f, "Instruction::DiffPositions({}, {})", old, new),
-            _ => write!(f, "Not implemented"),
-        }
-    }
-}
-
-fn get_compute_position_instructions<T, V>(
-    ids_to_values: &HashMap<T, V>,
-) -> HashMap<T, Instruction<'_, V>>
+fn get_compute_position_instructions<T, U>(
+    to_compute: &HashMap<T, U>,
+) -> HashMap<&T, ComputePosition<&U>>
 where
     T: Hash + Eq + Clone + Display,
 {
-    ids_to_values
+    to_compute
         .iter()
-        .map(|(key, value)| (key.clone(), Instruction::ComputePosition(value)))
+        .map(|(k, v)| (k, ComputePosition(v)))
         .collect()
 }
 
-fn get_diff_positions_instructions<'a, T, V>(
-    original_ids_to_positions: &'a HashMap<T, V>,
-    new_ids_to_positions: &'a HashMap<T, V>,
-) -> Result<HashMap<&'a T, Instruction<'a, V>>, Vec<&'a T>>
-where T: Hash + Eq + Display + Clone
+struct ComputePosition<T>(T);
+
+fn get_diff_positions_instructions<'a, T, U>(
+    original: &'a HashMap<T, U>,
+    new: &'a HashMap<T, U>,
+) -> Result<HashMap<T, DiffPositions<&'a U>>, HashMapDiffError<T>>
+where
+    T: Hash + Eq + Clone + Display,
 {
-    let mut problematic_keys = vec![];
+    check_hash_map_key_diffs(&original, &new)?;
 
-    let diff_positions_instructions = original_ids_to_positions.keys().map(|k| {
-        problematic_keys.push(k);
+    Ok(original
+        .keys()
+        .map(|k| {
+            let original = original.get(k).unwrap();
+            let new = new.get(k).unwrap();
 
-        let old_position = original_ids_to_positions.get(k)?;
-        let new_position = new_ids_to_positions.get(k)?;
+            (k.clone(), DiffPositions(original, new))
+        })
+        .collect())
+}
 
-        problematic_keys.pop();
+#[derive(Debug)]
+struct DiffPositions<T>(T, T);
 
-        return Some((k, Instruction::DiffPositions(old_position, new_position)));
-    }).collect::<Option<HashMap<&'a T, Instruction<'a, V>>>>();
+impl<T> PartialEq<DiffPositions<&T>> for DiffPositions<T>
+where
+    T: Eq,
+{
+    fn eq(&self, other: &DiffPositions<&T>) -> bool {
+        let DiffPositions(original, new) = self;
+        let DiffPositions(original_ref, new_ref) = other;
 
-    match diff_positions_instructions {
-        Some(diff_positions_instructions) => Ok(diff_positions_instructions),
-        None => Err(problematic_keys)
+        original == *original_ref && new == *new_ref
+    }
+}
+
+impl<T> PartialEq<DiffPositions<T>> for DiffPositions<T>
+where
+    T: Eq,
+{
+    fn eq(&self, other: &DiffPositions<T>) -> bool {
+        let DiffPositions(original, new) = self;
+        let DiffPositions(second_original, second_new) = other;
+
+        original == second_original && new == second_new
+    }
+}
+
+#[derive(Debug)]
+struct HashMapDiffError<T> {
+    present_in_original_but_not_new: HashSet<T>,
+    present_in_new_but_not_original: HashSet<T>,
+}
+
+impl<T> PartialEq for HashMapDiffError<T>
+where
+    T: Eq + Hash,
+{
+    fn eq(&self, other: &Self) -> bool {
+        let HashMapDiffError {
+            present_in_original_but_not_new: first_original,
+            present_in_new_but_not_original: second_original,
+        } = self;
+        let HashMapDiffError {
+            present_in_original_but_not_new: first_new,
+            present_in_new_but_not_original: second_new,
+        } = other;
+
+        first_original == first_new && second_original == second_new
+    }
+}
+
+fn check_hash_map_key_diffs<'a, T, U>(
+    original: &'a HashMap<T, U>,
+    new: &'a HashMap<T, U>,
+) -> Result<(), HashMapDiffError<T>>
+where
+    T: Hash + Eq + Clone + Display,
+{
+    let original_keys: HashSet<_> = original.keys().map(|t| t.clone()).collect();
+    let new_keys: HashSet<_> = new.keys().map(|t| t.clone()).collect();
+
+    if original_keys == new_keys {
+        Ok(())
+    } else {
+        let present_in_original_but_not_new = original_keys
+            .difference(&new_keys)
+            .map(|t| t.clone())
+            .collect();
+        let present_in_new_but_not_original = new_keys
+            .difference(&original_keys)
+            .map(|t| t.clone())
+            .collect();
+
+        Err(HashMapDiffError {
+            present_in_original_but_not_new,
+            present_in_new_but_not_original,
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
-    use crate::get_compute_position_instructions;
-    use crate::get_diff_positions_instructions;
-    use crate::Instruction;
+    use crate::{
+        check_hash_map_key_diffs, get_compute_position_instructions,
+        get_diff_positions_instructions, ComputePosition, DiffPositions, HashMapDiffError,
+    };
+    use std::collections::{HashMap, HashSet};
 
     #[test]
-    fn get_compute_position_instructions_for_hash_map_returns_new_hash_map_that_has_all_given_items(
-    ) {
-        let ids_to_values_vec = vec![
-            HashMap::from([("a", 0), ("b", 1), ("c", 2)]),
-            HashMap::from([]),
+    fn get_compute_position_instructions_returns_all_given_values() {
+        let first_hash_map: HashMap<&'static str, i32> = HashMap::from([]);
+        let second_hash_map: HashMap<&'static str, i32> =
+            HashMap::from([("a", 0), ("b", 2), ("c", 4)]);
+        let third_hash_map: HashMap<&'static str, i32> = HashMap::from([("t", 10), ("u", 20)]);
+
+        let hash_maps = vec![first_hash_map, second_hash_map, third_hash_map];
+
+        for hash_map in hash_maps {
+            let result = get_compute_position_instructions(&hash_map);
+
+            hash_map.iter().for_each(|(key, original_value)| {
+                let ComputePosition(value) = result.get(key).expect("Key {k} should be in results");
+
+                assert_eq!(*value, original_value);
+            });
+        }
+    }
+
+    #[test]
+    fn get_diff_positions_instructions_returns_ok_when_hash_maps_have_same_keys() {
+        let first_hash_map = HashMap::from([("a", 10), ("b", 20), ("c", 30)]);
+        let second_hash_map = HashMap::from([("a", 40), ("b", 50), ("c", 60)]);
+
+        if let Err(_) = get_diff_positions_instructions(&first_hash_map, &second_hash_map) {
+            panic!("Diffing hash maps with same keys should not return an error");
+        }
+    }
+
+    #[test]
+    fn get_diff_positions_instructions_returns_error_when_hash_maps_have_different_keys() {
+        let first_hash_map = HashMap::from([("a", 0), ("b", 1), ("c", 2)]);
+        let second_hash_map = HashMap::from([("d", 0), ("b", 2), ("c", 3)]);
+
+        if let Ok(_) = get_diff_positions_instructions(&first_hash_map, &second_hash_map) {
+            panic!("Diffing hash maps with different keys should return an error");
+        }
+    }
+
+    #[test]
+    fn get_diff_positions_instructions_returns_instructions_for_all_key_pairs() {
+        let first_hash_map = HashMap::from([("a", 0), ("b", 10), ("c", 20)]);
+        let second_hash_map = HashMap::from([("a", 0), ("b", 100), ("c", 200)]);
+
+        match get_diff_positions_instructions(&first_hash_map, &second_hash_map) {
+            Ok(diffs) => {
+                first_hash_map.keys().for_each(|k| {
+                    let diff = diffs.get(k).unwrap();
+
+                    let original = first_hash_map.get(k).unwrap();
+                    let new = second_hash_map.get(k).unwrap();
+
+                    assert_eq!(diff, &DiffPositions(original, new));
+                });
+            }
+            Err(diffs) => {
+                panic!(
+                    "No errors should have been returned, but {:?} key differences were reported",
+                    diffs
+                );
+            }
+        }
+    }
+
+    fn compare_hash_map_errors<T>(make_hash_map: T)
+    where
+        T: for<'a> Fn(
+                &'a HashMap<&'static str, i32>,
+                &'a HashMap<&'static str, i32>,
+            ) -> Result<HashMap<&'static str, DiffPositions<&'a i32>>, HashMapDiffError<&'static str>>
+    {
+        let first_hash_map_pair = (
+            HashMap::from([("a", 0), ("b", 1)]),
+            HashMap::from([("c", 0), ("b", 2)]),
+        );
+        let second_hash_map_pair = (
+            HashMap::from([("a", 0), ("b", 2)]),
+            HashMap::from([("a", 1), ("b", 2), ("c", 3)]),
+        );
+        let third_hash_map_pair = (
+            HashMap::from([("b", 0), ("c", 10)]),
+            HashMap::from([("a", 0), ("c", 20)]),
+        );
+
+        let hash_map_pairs = [
+            first_hash_map_pair,
+            second_hash_map_pair,
+            third_hash_map_pair
         ];
 
-        for ids_to_values in ids_to_values_vec {
-            let instructions = get_compute_position_instructions(&ids_to_values);
-
-            ids_to_values.iter().for_each(|(key, value)| {
-                let instruction = instructions.get(key).expect("Was unable to obtain instruction for {key}");
-
-                if let Instruction::ComputePosition(instruction_value) = instruction {
-                    assert_eq!(instruction_value, &value);
-                } else {
-                    panic!("Only position computing instructions were expected, but, for key {key}, instruction was {instruction}");
+        hash_map_pairs.iter().for_each(|(original, new)| {
+            match (
+                check_hash_map_key_diffs(original, new),
+                make_hash_map(original, new),
+            ) {
+                (Err(first_diff), Err(second_diff)) => {
+                    assert_eq!(first_diff, second_diff);
                 }
-            });
+                (Ok(_), Err(diffs)) => {
+                    panic!("get_diff_positions_instructions returned error whilst check_hash_map_key_diffs did not. {:?} was the returned error", diffs);
+                }
+                (Err(diffs), Ok(_)) => {
+                    panic!("get_diff_positions_instructions did not return an error whilst check_hash_map_key_diffs did. {:?} was the returnedd error", diffs);
+                }
+                (Ok(_), Ok(_)) => {
+                    panic!(
+                        "Both functions returned OK although errors were supposed to be returned"
+                    );
+                }
+            }
+        });
+    }
+
+    #[test]
+    fn get_diff_positions_instructions_returns_check_hash_map_key_diffs_error() {
+        compare_hash_map_errors(|original, new| {
+            Ok(get_diff_positions_instructions(original, new)?)
+        });
+    }
+
+    #[test]
+    fn check_hash_maps_key_diffs_returns_error_when_keys_differ() {
+        let first_hash_map = HashMap::from([("a", 0), ("b", 1)]);
+        let second_hash_map = HashMap::from([("c", 0), ("b", 1)]);
+
+        if let Ok(_) = check_hash_map_key_diffs(&first_hash_map, &second_hash_map) {
+            panic!("The function should return an error if hash maps have different keys");
         }
     }
 
     #[test]
-    fn get_diff_position_instructions_for_hash_maps_return_new_hash_map_that_has_all_given_items() {
-        let old_positions = HashMap::from([("a", 0), ("b", 1), ("c", 2)]);
-        let new_positions = HashMap::from([("a", 1), ("b", 2), ("c", 3)]);
+    fn check_hash_map_key_diffs_returns_ok_when_keys_are_same() {
+        let first_hash_map = HashMap::from([("a", 0), ("b", 1)]);
+        let second_hash_map = HashMap::from([("a", 10), ("b", 20)]);
 
-        let instructions = get_diff_positions_instructions(&old_positions, &new_positions).expect("Was unable to get instructions");
-
-        instructions.iter().for_each(|(k, v)| {
-            let instruction = instructions.get(*k).expect("Was unable to obtain instruction for {k}");
-
-            if let Instruction::DiffPositions(old, new) = instruction {
-                assert_eq!(old_positions.get(*k).expect("Was unable to get old position for {k}"), *old);
-                assert_eq!(new_positions.get(*k).expect("Was unable to get new position for {k}"), *new);
-            } else {
-                panic!("Only position diffing instructions were expected, but, for key {k}, instruction was {instruction}");
-            }
-        });
-
-
-        let old_positions = HashMap::from([("a", 0), ("b", 1)]);
-        let new_positions = HashMap::from([("a", 0)]);
-
-        if let Err(ks) = get_diff_positions_instructions(&old_positions, &new_positions) {
-            let mut contains_b = false;
-
-            ks.into_iter().for_each(|s| {
-                if *s.to_string() == "b".to_string() {
-                    contains_b = true;
-                }
-            });
-
-            assert!(contains_b);
-        } else {
-            panic!("Method call should have returned and error");
+        if let Err(_) = check_hash_map_key_diffs(&first_hash_map, &second_hash_map) {
+            panic!("The function should not report errors if keys are same");
         }
     }
+
+    #[test]
+    fn check_hash_map_key_diffs_returns_error_with_original_keys_missing_in_new() {
+        let first_hash_map = HashMap::from([("a", 1), ("b", 2), ("c", 3)]);
+        let second_hash_map = HashMap::from([("a", 1)]);
+
+        match check_hash_map_key_diffs(&first_hash_map, &second_hash_map) {
+            Ok(_) => panic!(
+                "As keys from original are missing in new, an error should have been returned"
+            ),
+            Err(HashMapDiffError {
+                present_in_original_but_not_new,
+                ..
+            }) => {
+                let expected = HashSet::from(["b", "c"]);
+
+                assert_eq!(expected, present_in_original_but_not_new);
+            }
+        }
+    }
+
+    #[test]
+    fn check_hash_map_key_diff_returns_error_with_new_keys_missing_in_original() {
+        let first_hash_map = HashMap::from([("a", 1)]);
+        let second_hash_map = HashMap::from([("a", 2), ("b", 3), ("c", 4)]);
+
+        match check_hash_map_key_diffs(&first_hash_map, &second_hash_map) {
+            Ok(_) => panic!(
+                "As keys from new are missing in original, an error should have been returneed"
+            ),
+            Err(HashMapDiffError {
+                present_in_new_but_not_original,
+                ..
+            }) => {
+                let expected = HashSet::from(["b", "c"]);
+
+                assert_eq!(expected, present_in_new_but_not_original);
+            }
+        }
+    }
+
+    // #[test]
+    // fn get_set_transform_and_transition_instructions
 }
